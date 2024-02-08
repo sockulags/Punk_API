ca2786dbc8a3f3ba50b2c424821ba5a5d8f81ae2
Lucas Skog <lucasskog@gmail.com>
2024-02-08 15:07:51 +0100

    Added info page

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const nextBeerBtn = document.querySelector(".right-arrow-container");
 const audio = document.querySelector("audio");
 const readMoreBtn = document.getElementById("read_more_button");
 const randomBeerPage = document.querySelector(".random-beer-page");
+const beerInfoContainer = document.querySelector('.beer-info-container');
+const randomBeerContainer = document.querySelector('.random-beer-page');
 
 const nextBeerSound = "another-one_dPvHt2Z.mp3";
 const prevBeerSound = "ben-yes-made-with-Voicemod.mp3";
+let currentImgBgColor = "";
 
 const viewedBeers = [];
 const cachedBeers = {};
@@ -25,16 +28,37 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(loadPreviousBeer(), 500);
   }); 
 
-  container.addEventListener("click", (e) => {
-    if(e.target.parentElement.id == "read_more_button"){
-        showBeerDetails();
+  
+  beerInfoContainer.addEventListener("mouseover", () => {
+    document.querySelector('.exit').classList.add('show');
+  })
+  beerInfoContainer.addEventListener("mouseleave", () => {
+    document.querySelector('.exit').classList.remove('show');
+  })
+  beerInfoContainer.addEventListener("click", (e)=> {
+    if(e.target === document.querySelector('.exit')){
+      hideBeerDetails()
     }
-
   })
 });
 
+
+container.addEventListener("click", (e) => {
+  if(e.target.parentElement.id === "read_more_button"){
+      showBeerDetails();
+  }
+})
 function showBeerDetails() {
   console.log("Button is working")
+  const beer = viewedBeers[viewedBeers.length-1];
+  console.log(cachedBeers[beer])
+  beerInfoContainer.innerHTML = beerInfoCard(cachedBeers[beer]);
+  randomBeerContainer.style.display = "none";  
+}
+
+function hideBeerDetails(){
+  beerInfoContainer.innerHTML = "";
+  randomBeerContainer.style.display = "";  
 }
 
 function showNextBeerBtn() {
@@ -44,8 +68,7 @@ function showNextBeerBtn() {
 function playSound(soundSrc) {
   audio.src = soundSrc;
   audio.volume = 0.05;
-  audio.play();
-  console.log(readMoreBtn);
+  audio.play(); 
 }
 
 async function fetchRandomBeer() {
@@ -83,7 +106,7 @@ function cacheBeers(beers) {
 
 function loadPreviousBeer() {
   viewedBeers.pop();
-  const previousBeerId = viewedBeers.pop();
+  const previousBeerId = viewedBeers[viewedBeers.length-1];
   if (viewedBeers.length < 2) {
     previousBeerBtn.classList.add("hider");
   }
@@ -98,6 +121,7 @@ function loadPreviousBeer() {
 function beerCard(randomBeer) {
   return `<div class="card" id="random-beer">
 <div class="card-image" style="background-color:${randomHexColor()}">
+
   <img src="${randomBeer.image_url ?? imageNotFound}"/>
 </div>
 <div class="card-text">
@@ -113,11 +137,39 @@ function beerCard(randomBeer) {
 `;
 }
 
+function beerInfoCard(beer) {
+  const foodPairings = beer.food_pairing.map(pairing => pairing + "\n").join('');
+  const maltIngredients = beer.ingredients.malt.map(m => m.name).join(', ');
+
+  return `
+    <div class="card">
+    <div class="card-image" style="background-color:${currentImgBgColor}">
+    <span class="material-symbols-outlined exit">close</span>
+  <img src="${beer.image_url ?? imageNotFound}"/>
+</div>  
+      <div class="beer-name">
+        <h2>${beer.name}</h2>
+        <p>${beer.tagline}</p> 
+      </div>                
+      <p class="beer-desc">${beer.description}</p>
+      <div class="beer-info">
+        <p>Alcohol by volume: ${beer.abv}%</p>
+        <p>Volume: 20litres</p>
+        <p>Hops: ${beer.ingredients.hops.map(hop => hop.name).join(', ')}</p>
+        <p>Food pairing: ${foodPairings}</p>
+        <p>Ingredients: ${maltIngredients}</p>
+      </div>
+      <p class="beer-tips">"This beer is all about the balance between the malty backbone of the beer and the fresh hop aroma. The fresher the hops the better."</p>
+    </div>`;
+}
+
+
 function randomHexColor() {
   var letters = "0123456789ABCDEF";
   var color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+  currentImgBgColor = color;
   return color;
 }
